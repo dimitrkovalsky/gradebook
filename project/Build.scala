15273a13220d7d8e2cab94ed13c0ed49aac8933c
dimitrkovalsky <dimitrkovalsky@rambler.ru>
2013-10-29 10:17:26 +0200

    Created project structure
    Used Play framework v 2.1.4

---
+++ project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "gradebook"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "com.novus" %% "salat" % "1.9.2",
+    jdbc
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+     resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  )
+
+}
