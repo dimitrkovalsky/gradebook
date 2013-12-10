136e39cec8f85d86c23f75299cde78c38ebc6e06
rithapai <maxkontarev@gmail.com>
2013-12-10 19:23:31 +0200

    preloader

---
+++ app/preloaders/DepartmentPreloader.scala
@@ -0,0 +1,19 @@
+package preloaders
+
+import models.{Lecturer, Department}
+import models.Predefined._
+import org.bson.types.ObjectId
+
+/**
+ * User: mkontarev
+ * Date: 12/10/13
+ * Time: 5:35 PM
+ */
+class DepartmentPreloader extends Preloader {
+  def preload(): String = {
+    val departments = List(Department("Компьютерні системі управління", institutes(1), new ObjectId(), "+380432123456", "+380432654321", "+380432123123", 1))
+    departments.foreach(Department.save)
+    s"[DepartmentPreloader] loaded   $${departments.size} departments"
+  }
+
+}
---
+++ app/preloaders/LecturerPreloader.scala
@@ -0,0 +1,10 @@
+package preloaders
+
+/**
+ * User: mkontarev
+ * Date: 12/10/13
+ * Time: 6:04 PM
+ */
+class LecturerPreloader {
+
+}
